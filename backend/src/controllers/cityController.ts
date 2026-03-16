c4106f44fd0acd63b69b97da12548b821c8877e9
idaballegaard <idabal01@easv365.dk>
2026-03-16 13:56:40 +0100

    feat: createCity function in cityController

--- backend/src/controllers/cityController.ts
+++ backend/src/controllers/cityController.ts
@@ -3,3 +3,27 @@ import { CityModel } from "../models/cityModel";
 import { connectionToDatabase, disconnectFromDatabase } from "../repository/database";
 import { buildDynamicQuery } from "./dynamicQueryBuilder";
 
+
+/**
+ * Add new CITY to the database
+ * @param req
+ * @param res
+ */
+export async function createCity(req: Request, res: Response): Promise<void> {
+  const data = req.body;
+
+  try {
+    await connectionToDatabase();
+
+    const city = new CityModel(data);
+    const result = await city.save();
+
+    res.status(201).json(result);
+  } catch (err) {
+    console.error("Error creating city:", err);
+
+    res.status(500).json("An error occurred while creating the city." + err);
+  } finally {
+    await disconnectFromDatabase();
+  }
+}
