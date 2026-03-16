de63c39bcffc5138deabade6707a28c5e6a08dfe
idaballegaard <idabal01@easv365.dk>
2026-03-16 14:00:28 +0100

    feat: getAllCities function in cityController

--- backend/src/controllers/cityController.ts
+++ backend/src/controllers/cityController.ts
@@ -10,20 +10,43 @@ import { buildDynamicQuery } from "./dynamicQueryBuilder";
  * @param res
  */
 export async function createCity(req: Request, res: Response): Promise<void> {
-  const data = req.body;
+    const data = req.body;
+    
+    try {
+        await connectionToDatabase();
 
-  try {
-    await connectionToDatabase();
+        const city = new CityModel(data);
+        const result = await city.save();
 
-    const city = new CityModel(data);
-    const result = await city.save();
+        res.status(201).json(result);
+    }
+    catch (err) {
+        console.error("Error creating city:", err);
 
-    res.status(201).json(result);
-  } catch (err) {
-    console.error("Error creating city:", err);
-
-    res.status(500).json("An error occurred while creating the city." + err);
-  } finally {
-    await disconnectFromDatabase();
+        res.status(500).json("An error occurred while creating the city." + err);
+    }
+    finally {
+        await disconnectFromDatabase();
   }
+}
+
+/**
+ * Retrieve all CITIES from the database
+ * @param req
+ * @param res
+ */
+export async function getAllCities(req: Request, res: Response) {
+    try {
+        await connectionToDatabase();
+
+        const result = await CityModel.find({});
+
+        res.status(200).json(result);
+    }
+    catch (err) {
+        res.status(500).json("error retrieving the cities." + err);
+    }
+    finally {
+        await disconnectFromDatabase();
+    }
 }
