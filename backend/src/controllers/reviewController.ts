ced4dd59e288d0947347e97e10ff0f7c556bb3d8
idaballegaard <idabal01@easv365.dk>
2026-03-19 10:35:34 +0100

    feat: createReview function in reviewController

--- backend/src/controllers/reviewController.ts
+++ backend/src/controllers/reviewController.ts
@@ -2,3 +2,30 @@ import { Request, Response } from "express";
 import { ReviewModel } from "../models/reviewModel";
 import { connectionToDatabase, disconnectFromDatabase } from "../repository/database";
 import { buildDynamicQuery } from "./dynamicQueryBuilder";
+
+
+/**
+ * Add new REVIEW to the database
+ * @param req
+ * @param res
+ */
+export async function createReview(req: Request, res: Response): Promise<void> {
+  const data = req.body;
+
+  try {
+    await connectionToDatabase();
+
+    const review = new ReviewModel(data);
+    const result = await review.save();
+
+    res.status(201).json(result);
+  }
+  catch (err) {
+    console.error("Error creating review:", err);
+
+    res.status(500).json("An error occurred while creating the review." + err);
+  }
+  finally {
+    await disconnectFromDatabase();
+  }
+}
