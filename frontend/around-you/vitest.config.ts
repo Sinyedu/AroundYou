3e7011e697e71f2e45b583c941d71f2d95e44a01
Simon Nyblom <simnyb01@easv365.dk>
2026-04-21 20:11:53 +0200

    chore: fixed vitest config, including installing the vitest depen for test coverage

--- frontend/around-you/vitest.config.ts
+++ frontend/around-you/vitest.config.ts
@@ -9,6 +9,12 @@ export default mergeConfig(
       environment: 'jsdom',
       exclude: [...configDefaults.exclude, 'e2e/**'],
       root: fileURLToPath(new URL('./', import.meta.url)),
+
+      coverage: {
+        provider: 'v8',
+        reporter: ['text', 'html', 'lcov'],
+        exclude: ['node_modules/', 'e2e/', 'dist/', '**/*.d.ts'],
+      },
     },
   }),
 )
