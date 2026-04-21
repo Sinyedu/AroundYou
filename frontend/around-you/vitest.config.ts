a245fd3677e4c7773531f550078a721e2153f8d5
Simon Nyblom <simnyb01@easv365.dk>
2026-04-21 20:33:54 +0200

    chore: remade vitestconfig

--- frontend/around-you/vitest.config.ts
+++ frontend/around-you/vitest.config.ts
@@ -1,16 +1,17 @@
 import { fileURLToPath } from 'node:url'
-import { defineConfig, configDefaults } from 'vitest/config'
+import { defineConfig, mergeConfig } from 'vitest/config'
+import viteConfig from './vite.config'
 
-export default defineConfig({
-  test: {
-    environment: 'jsdom',
-    exclude: [...configDefaults.exclude, 'e2e/**'],
-    root: fileURLToPath(new URL('./', import.meta.url)),
-
-    coverage: {
-      provider: 'v8',
-      reporter: ['text', 'html', 'lcov'],
-      exclude: ['node_modules/', 'e2e/', 'dist/', '**/*.d.ts'],
+export default mergeConfig(
+  viteConfig,
+  defineConfig({
+    test: {
+      environment: 'jsdom',
+      root: fileURLToPath(new URL('./', import.meta.url)),
+      coverage: {
+        provider: 'v8',
+        reporter: ['text', 'html', 'lcov'],
+      },
     },
-  },
-})
+  }),
+)
