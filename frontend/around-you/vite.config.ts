b61172493202357d177b99c7a9fd427e47f6a846
Simon Nyblom <simnyb01@easv365.dk>
2026-04-21 20:26:03 +0200

    chore: idek anymore

--- frontend/around-you/vite.config.ts
+++ frontend/around-you/vite.config.ts
@@ -1,20 +1,13 @@
 import { fileURLToPath, URL } from 'node:url'
+
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import tailwindcss from '@tailwindcss/vite'
 
-const isTest = process.env.VITEST === 'true'
-
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-
-    //Skips tailwind in testing
-    !isTest && tailwindcss(),
-  ].filter(Boolean),
-
+  plugins: [vue(), vueDevTools(), tailwindcss()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
