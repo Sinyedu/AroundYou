07b6fdf7e06273072713b9e284462fe298e91ecd
Simon Nyblom <simnyb01@easv365.dk>
2026-04-21 20:22:18 +0200

    chore: made so tailwind in testing isn't interfering with CI pipeline

--- frontend/around-you/vite.config.ts
+++ frontend/around-you/vite.config.ts
@@ -1,13 +1,20 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
+const isTest = process.env.VITEST === 'true'
+
 export default defineConfig({
-  plugins: [vue(), vueDevTools(), tailwindcss()],
+  plugins: [
+    vue(),
+    vueDevTools(),
+
+    //Skips tailwind in testing
+    !isTest && tailwindcss(),
+  ].filter(Boolean),
+
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
--- frontend/around-you/vitest.config.ts
+++ frontend/around-you/vitest.config.ts
@@ -10,6 +10,10 @@ export default mergeConfig(
       exclude: [...configDefaults.exclude, 'e2e/**'],
       root: fileURLToPath(new URL('./', import.meta.url)),
 
+      env: {
+        VITEST: 'true',
+      },
+
       coverage: {
         provider: 'v8',
         reporter: ['text', 'html', 'lcov'],
