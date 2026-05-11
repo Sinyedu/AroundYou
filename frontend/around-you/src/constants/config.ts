088165d66ff8baa8c6dc3f820c74ebbfa823f685
Simon Erik Nyblom <nyblomsimon@gmail.com>
2026-05-11 10:44:42 +0200

    feat: made more stuff be able to work locally and on our prod

--- frontend/around-you/src/constants/config.ts
+++ frontend/around-you/src/constants/config.ts
@@ -1,3 +1,10 @@
-export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL ?? 'http://localhost:4000/api'
+const DEFAULT_API_BASE_URL = 'http://localhost:4000/api'
+const configuredApiBaseUrl = import.meta.env.VITE_API_BASE_URL
+
+if (import.meta.env.PROD && !configuredApiBaseUrl) {
+  throw new Error('Missing VITE_API_BASE_URL for production build')
+}
+
+export const API_BASE_URL = (configuredApiBaseUrl ?? DEFAULT_API_BASE_URL).replace(/\/$/, '')
 
 export const USER_API_URL = `${API_BASE_URL}/user`
